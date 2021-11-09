f3f94f279d88993c691ddaa0b3c3446e2a3fdb3b
Tay <tmitsumori96@gmail.com>
2021-11-08 21:04:08 -0800

    User can see all playlists

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+] 
+
 @app.route('/')
-def index():
-    """Return homepage."""
-    # change the original return statement you wrote to the one below
-    return render_template('home.html', msg='Flask is Cool!!')
+def playlists_index():
+    """Show all playlists."""
+    return render_template('playlists_index.html', playlists=playlists.find())
+    
 if __name__ == '__main__':
     app.run(debug=True)
 
